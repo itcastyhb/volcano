34f457a2ecfb11ed33f60c40984e65a5febee6cb
volcano <412272566@qq.com>
2018-12-18 15:38:32 +0800

    计数 count

--- hello.java
+++ hello.java
@@ -1,8 +1,10 @@
 public class hello{
 	public static void main(String[] args){
 		System.out.println("hello world");
-		for(int i = 0; i<10;i++){
+		int count  = 0;
+		for(int i = 0; i < 10; i++){
 			System.out.println("hello world"+i);
+			count++;
 		}
 	}
 }
